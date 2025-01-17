a3c65073b8255591425ea85e94cd5a6e620f304d
Laksmaprabhu <41356433+Laksmaprabhu@users.noreply.github.com>
2025-01-17 16:31:56 +0530

    Update studentSlice.js

--- learning-academy/client/academy/src/slices/studentSlice.js
+++ learning-academy/client/academy/src/slices/studentSlice.js
@@ -1,1 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    userId: '', // Default empty string
+    username: '',
+    email: '', // Added email for more user details
+};
+
+const studentSlice = createSlice({
+    name: 'student',
+    initialState,
+    reducers: {
+        setUserData: (state, action) => {
+            state.userId = action.payload.userId;
+            state.username = action.payload.username;
+            state.email = action.payload.email;
+        },
+    },
+});
+
+export const { setUserData } = studentSlice.actions;
+export default studentSlice.reducer;
 
